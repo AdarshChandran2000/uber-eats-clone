ffb405f5eedc961423c69995b6fff8f366fbaf7c
Adarsh Shinju Chandran <lachikku2210@gmail.com>
2021-09-21 14:46:01 +0300

    Added HeaderTabs component

---
+++ screens/Home.js
@@ -0,0 +1,12 @@
+// used rnf to start
+import React from 'react'
+import { View, Text, SafeAreaView } from 'react-native'
+import HeaderTabs from '../components/HeaderTabs'
+
+export default function Home() {
+    return (
+        <SafeAreaView>
+            <HeaderTabs/>
+        </SafeAreaView>
+    )
+}

a6eb49cef63c82afbf006de68eb48c3663381fb1
Adarsh Shinju Chandran <lachikku2210@gmail.com>
2021-09-21 15:41:20 +0300

    Added Google API Search Bar UI

--- screens/Home.js
+++ screens/Home.js
@@ -2,11 +2,20 @@
 import React from 'react'
 import { View, Text, SafeAreaView } from 'react-native'
 import HeaderTabs from '../components/HeaderTabs'
+import SearchBar from '../components/SearchBar';
 
 export default function Home() {
     return (
-        <SafeAreaView>
-            <HeaderTabs/>
+        // flex : 1 makes the entire screen grey
+        <SafeAreaView style={{ backgroundColor: '#eee', flex: 1 }}>
+            <View style={{ backgroundColor: 'white', padding: 15 }}>
+                <HeaderTabs />
+                <SearchBar />
+            </View>
         </SafeAreaView>
-    )
+    );
 }
+
+// added 2 packages for searchbar
+// react-native-vector-icons
+// react-native-google-places-autocomplete

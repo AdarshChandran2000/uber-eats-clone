dd8fea2934aa393f8de11f77d466fd6089c6030b
Adarsh Shinju Chandran <lachikku2210@gmail.com>
2021-09-22 03:21:47 +0300

    Code design changes

--- App.js
+++ App.js
@@ -3,7 +3,5 @@ import { View, Text } from "react-native";
 import Home from "./screens/Home";
 
 export default function App() {
-  return (
-    <Home/>  
-  );
+  return <Home />;
 }
--- components/RestaurantItems.js
+++ components/RestaurantItems.js
@@ -1,93 +1,100 @@
 //rnf
-import React from 'react'
-import { View, Text, Image, TouchableOpacity } from 'react-native'
-import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
+import React from "react";
+import { View, Text, Image, TouchableOpacity } from "react-native";
+import MaterialCommunityIcons from "react-native-vector-icons/MaterialCommunityIcons";
 
 export const localRestaurants = [
-    {
-        name: "Beachside Bar",
-        image_url: "https://www.bahrainyellow.com/img/cats/restaurants.jpg",
-        cateogries: ['Cafe', 'Bar'],
-        price: "$$",
-        reviews: 1244,
-        rating: 4.5,
-    },
-    {
-        name: "Benihana",
-        image_url: "https://www.bahrainyellow.com/img/cats/restaurants.jpg",
-        cateogries: ['Cafe', 'Bar'],
-        price: "$$",
-        reviews: 1244,
-        rating: 3.7,
-    },
-    {
-        name: "India's Grill",
-        image_url: "https://www.bahrainyellow.com/img/cats/restaurants.jpg",
-        cateogries: ['Indian', 'Bar'],
-        price: "$$",
-        reviews: 1546,
-        rating: 4.9,
-    }
-]
+  {
+    name: "Beachside Bar",
+    image_url: "https://www.bahrainyellow.com/img/cats/restaurants.jpg",
+    cateogries: ["Cafe", "Bar"],
+    price: "$$",
+    reviews: 1244,
+    rating: 4.5,
+  },
+  {
+    name: "Benihana",
+    image_url: "https://www.bahrainyellow.com/img/cats/restaurants.jpg",
+    cateogries: ["Cafe", "Bar"],
+    price: "$$",
+    reviews: 1244,
+    rating: 3.7,
+  },
+  {
+    name: "India's Grill",
+    image_url: "https://www.bahrainyellow.com/img/cats/restaurants.jpg",
+    cateogries: ["Indian", "Bar"],
+    price: "$$",
+    reviews: 1546,
+    rating: 4.9,
+  },
+];
 
 // activeOpacity=1 to remove the blinking effect
 export default function RestaurantItems(props) {
-    return (
-        <TouchableOpacity activeOpacity={1} style={{marginBottom: 30}}>
-            {props.restaurantsData.map((restaurant, index) => (
-                <View 
-                key={index} 
-                style={{
-                    marginTop: 10,
-                    padding: 15,
-                    backgroundColor: 'white',
-                }}>
-                    <RestaurantImage image={restaurant.image_url}/>
-                    <RestaurantInfo name={restaurant.name} rating={restaurant.rating}/>
-                </View>
-            ))}
-        </TouchableOpacity>
-    )
+  return (
+    <TouchableOpacity activeOpacity={1} style={{ marginBottom: 30 }}>
+      {props.restaurantsData.map((restaurant, index) => (
+        <View
+          key={index}
+          style={{
+            marginTop: 10,
+            padding: 15,
+            backgroundColor: "white",
+          }}
+        >
+          <RestaurantImage image={restaurant.image_url} />
+          <RestaurantInfo name={restaurant.name} rating={restaurant.rating} />
+        </View>
+      ))}
+    </TouchableOpacity>
+  );
 }
 
 const RestaurantImage = (props) => (
-    <>
+  <>
     <Image
       source={{
-          uri: props.image
+        uri: props.image,
       }}
-      style={{width: "100%", height: 180}}
+      style={{ width: "100%", height: 180 }}
     />
-    <TouchableOpacity style={{
-        position: 'absolute',
+    <TouchableOpacity
+      style={{
+        position: "absolute",
         right: 20,
         top: 20,
-    }}>
-        <MaterialCommunityIcons name='heart-outline' size={25} color='#fff'/>
+      }}
+    >
+      <MaterialCommunityIcons name="heart-outline" size={25} color="#fff" />
     </TouchableOpacity>
-    </>
+  </>
 );
 
 const RestaurantInfo = (props) => (
-    <View style={{
-        flexDirection: 'row',
-        justifyContent: 'space-between',
-        alignItems: 'center',
-        marginTop: 10
-    }}>
-        <View>
-            <Text style={{ fontSize: 15, fontWeight: 'bold' }}>{props.name}</Text>
-            <Text style={{ fontSize: 13, color: 'grey' }}>30-45 · min</Text>
-        </View>
-        <View  style={{ 
-            backgroundColor: '#eee',
-            height: 30, 
-            width: 30, 
-            alignItems: 'center',
-            justifyContent: 'center',
-            borderRadius: 15,
-        }}>
-            <Text>{props.rating}</Text>
-        </View>
+  <View
+    style={{
+      flexDirection: "row",
+      justifyContent: "space-between",
+      alignItems: "center",
+      marginTop: 10,
+    }}
+  >
+    <View>
+      <Text style={{ fontSize: 15, fontWeight: "bold" }}>{props.name}</Text>
+      <Text style={{ fontSize: 13, color: "grey" }}>30-45 · min</Text>
     </View>
-)
+    <View
+      style={{
+        backgroundColor: "#eee",
+        height: 30,
+        width: 30,
+        alignItems: "center",
+        justifyContent: "center",
+        borderRadius: 15,
+      }}
+    >
+      <Text>{props.rating}</Text>
+    </View>
+  </View>
+);
